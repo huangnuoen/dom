e0973ee626fbf4b795e9f7f6a0483b76e9993c55
huangnuoen <1261290560@qq.com>
2017-06-20 22:26:30 +0800

    听指令的方块二

--- ife4-4/chess.js
+++ ife4-4/chess.js
@@ -1,5 +1,5 @@
 //前进
-function go () {
+function go (direct) {
 	var box = document.getElementById('box');
 	var top = box.offsetTop;
 	var left = box.offsetLeft;
@@ -20,32 +20,67 @@ function go () {
 var a = document.getElementById('a');
 //a.onclick = go;
 a.addEventListener('click', function(e){
-	go();
+	getOrder();
 	e.preventDefault();//取消默认表单提交
 } , false);
 
 var direct = 0;//方向
-//改变方向
-function changeDirect() {
-	var b = document.getElementById('b');
-	b.onclick = function() {
-		direct = direct > 0 ? (--direct) : (direct + 3);
-		console.log(direct);
-		changeStyle(direct);
-		return false;
-	}
-	var c = document.getElementById('c');
-	c.onclick = function() {
-		direct += 1;
-		direct = direct < 4 ? direct : (direct%4);
-		console.log(direct);
-		changeStyle(direct);
-		return false;
-	}
+/*//改变方向
+//左转
+var gLeft = function(d) {
+	d = d > 0 ? (--d) : (d + 3);
+	console.log(d);
+	changeStyle(d);
 }
+//右转
+var gRight = function(d) {
+	d += 1;
+	d = d < 4 ? d : (d%4);
+	console.log(d);
+	changeStyle(d);
+}*/
+
 //改变朝向
 function changeStyle(d) {
 	var box = document.getElementById('box');
 	box.style.transform = 'rotate(0' + d * 90 + 'deg)';
 }
-changeDirect();
+//获取input值,并执行相应函数
+function getOrder() {
+	var order = document.getElementById('order');
+	var val = order.value.toLowerCase();
+	console.log(val);
+	var box = document.getElementById('box');
+	var top = box.offsetTop;
+	var left = box.offsetLeft;
+	switch(val) {
+		case 'tra lef':
+			box.style.left = (left > 40) ? (left - 40 + 'px') : (left + 'px');
+			break;
+		case 'tra top':
+			box.style.top = (top > 40) ? (top - 40 + 'px') : (top + 'px');
+			break;
+		case 'tra rig':
+			box.style.left = (left < 400) ? (left + 40 + 'px') : (left + 'px');
+			break;
+		case 'tra bot':
+			box.style.top = (top < 400) ? (top + 40 + 'px') : (top + 'px');
+			break;			
+		case 'mov lef':
+			changeStyle(3);
+			setTimeout(function(){go(3)}, 1000);//设置定时器，以便看到先旋转再移动的过程
+			break;
+		case 'mov top':
+			changeStyle(0);
+			setTimeout(function(){go(0)}, 1000);
+			break;
+		case 'mov rig':
+			changeStyle(1);
+			setTimeout(function(){go(1)}, 1000);
+			break;
+		case 'mov bot':
+			changeStyle(2);
+			setTimeout(function(){go(2)}, 1000);
+			break
+	}
+}
--- ife4-4/readme.txt
+++ ife4-4/readme.txt
@@ -1,8 +1,9 @@
 ﻿https://huangnuoen.github.io/dom/ife4-4/chess.html
 
 1、如何判断棋子方向
-点击左转时，变量face改变，记录当前方向，同时进行旋转
+点击左转时，变量direct改变，记录当前方向，同时进行旋转
 2、如何移动
 设置position
 3、如何判断边界
 用坐标判断
+4、根据input值去执行相应指令
